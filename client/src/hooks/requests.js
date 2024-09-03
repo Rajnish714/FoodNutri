66e01331f997c7b56ce7b5938b98323687b70d2e
GameRix <srajnish714@gmail.com>
2024-09-04 02:02:23 +0530

    request getfood has been added

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,5 +1,13 @@
 const API_URL = "http://localhost:8000";
 
-async function httpsGetAllFood() {
+async function httpsGetFood() {
+  try {
+    const response = await fetch(`${API_URL}/getfood`);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+
   //to:do-add fetch get food api
 }
